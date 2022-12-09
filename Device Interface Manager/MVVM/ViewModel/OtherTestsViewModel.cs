98fd264932436c1e7c1d706daa118a972add81f5
Oudoum <77352752+Oudoum@users.noreply.github.com>
2022-12-09 19:15:27 +0100

    Cleanup OtherTestsViewModel

--- Device Interface Manager/MVVM/ViewModel/OtherTestsViewModel.cs
+++ Device Interface Manager/MVVM/ViewModel/OtherTestsViewModel.cs
@@ -8,270 +8,176 @@ using static Device_Interface_Manager.MVVM.ViewModel.MainViewModel;
 
 namespace Device_Interface_Manager.MVVM.ViewModel
 {
-    class OtherTestsViewModel : ObservableObject
+    [INotifyPropertyChanged]
+    partial class OtherTestsViewModel
     {
-
         public CancellationTokenSource GetValuesCancellationTokenSource { get; set; }
-
-        public RelayCommand SevenSegmentEnableCommand { get; set; }
-
-        public RelayCommand DatalineEnableCommand { get; set; }
-
-        public RelayCommand BrightnessEnableCommand { get; set; }
-
-        public RelayCommand AnalogEnableCommand { get; set; }
-
-        public RelayCommand LoggingEnableCommand { get; set; }
-
-
 
         public Thread AnalogValueThread { get; set; }
 
+        public ObservableCollection<int> SevenSegmentPositions { get; set; } = new();
 
-        public ObservableCollection<int> SevenSegmentPositions { get; set; } = new ObservableCollection<int>();
-
-
+        [ObservableProperty]
         private bool _sevenSegmentEnabled;
-        public bool SevenSegmentEnabled
-        {
-            get => this._sevenSegmentEnabled;
-            set
-            {
-                if (value == true)
-                {
-                    this.SevenSegmentPositions.Clear();
-                    for (int i = DeviceList[GetSeletedController()].DeviceInfo.n7SegmentFirst; i <= DeviceList[GetSeletedController()].DeviceInfo.n7SegmentLast; i++)
-                    {
-                        this.SevenSegmentSelectedPosition = 1;
-                        this.SevenSegmentPositions.Add(i);
-                    }
-                }
-                InterfaceITAPI_Data.interfaceIT_7Segment_Enable(GetSelectedDeviceSession(), value);
-                this._sevenSegmentEnabled = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(SevenSegmentText))]
         private int _sevenSegmentSelectedPosition;
-        public int SevenSegmentSelectedPosition
+        partial void OnSevenSegmentSelectedPositionChanged(int value)
         {
-            get => this._sevenSegmentSelectedPosition;
-            set
-            {
-                this._sevenSegmentSelectedPosition = value;
-                Reset7SegmentDisplay();
-                this.SevenSegmentText = this.SevenSegmentText;
-                OnPropertyChanged();
-            }
+            Reset7SegmentDisplay();
+            OnSevenSegmentTextChanged(_sevenSegmentText);
         }
 
+        [ObservableProperty]
         private string _sevenSegmentText = string.Empty;
-        public string SevenSegmentText
+        partial void OnSevenSegmentTextChanging(string value)
         {
-            get => this._sevenSegmentText;
-            set
+            if (this.SevenSegmentText.Length > value.Length)
             {
-                if (value.Length <= DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount - this.SevenSegmentSelectedPosition + 1)
-                {
-                    if (_sevenSegmentText.Length > value.Length)
-                    {
-                        Reset7SegmentDisplay();
-                    }
-                    this._sevenSegmentText = value;
-                }
-                else
-                {
-                    this._sevenSegmentText = value.Remove(DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount - this.SevenSegmentSelectedPosition + 1);
-                    OnPropertyChanged();
-                }
-                    InterfaceITAPI_Data.interfaceIT_7Segment_Display(GetSelectedDeviceSession(), this.SevenSegmentText, this.SevenSegmentSelectedPosition);
-                    OnPropertyChanged();
+                Reset7SegmentDisplay();
             }
         }
-
-        public static void Reset7SegmentDisplay()
+        partial void OnSevenSegmentTextChanged(string value)
         {
-            InterfaceITAPI_Data.interfaceIT_7Segment_Display(GetSelectedDeviceSession(), new string(' ', DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount), 1);
+            if (value.Length > DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount - this.SevenSegmentSelectedPosition + 1)
+            {
+                this._sevenSegmentText = value.Remove(DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount - this.SevenSegmentSelectedPosition + 1);
+                _ = InterfaceITAPI_Data.interfaceIT_7Segment_Display(GetSelectedDeviceSession(), this._sevenSegmentText, this.SevenSegmentSelectedPosition);
+                return;
+            }
+            _ = InterfaceITAPI_Data.interfaceIT_7Segment_Display(GetSelectedDeviceSession(), this._sevenSegmentText = value, this.SevenSegmentSelectedPosition);
         }
 
+        [ObservableProperty]
         private bool _datalineEnabled;
-        public bool DatalineEnabled
-        {
-            get => this._datalineEnabled;
-            set
-            {
-                if (value == true)
-                {
-                    InterfaceITAPI_Data.interfaceIT_Dataline_Enable(GetSelectedDeviceSession(), value);
-                    InterfaceITAPI_Data.interfaceIT_Dataline_Set(GetSelectedDeviceSession(), DeviceList[GetSeletedController()].DeviceInfo.nDatalineFirst, value);
-                }
-                else
-                {
-                    InterfaceITAPI_Data.interfaceIT_Dataline_Set(GetSelectedDeviceSession(), DeviceList[GetSeletedController()].DeviceInfo.nDatalineFirst, value);
-                    InterfaceITAPI_Data.interfaceIT_Dataline_Enable(GetSelectedDeviceSession(), value);
-                }
-                this._datalineEnabled = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
         private int _brightnessValue;
-        public int BrightnessValue
+        partial void OnBrightnessValueChanged(int value)
         {
-            get => this._brightnessValue;
-            set
-            {
-                this._brightnessValue = value;
-                InterfaceITAPI_Data.interfaceIT_Brightness_Set(GetSelectedDeviceSession(), value);
-                OnPropertyChanged();
-            }
+            _ = InterfaceITAPI_Data.interfaceIT_Brightness_Set(GetSelectedDeviceSession(), value);
         }
 
+        [ObservableProperty]
         private bool _brightnessEnabled;
-        public bool BrightnessEnabled
-        {
-            get => this._brightnessEnabled;
-            set
-            {
-                this.BrightnessValue = 0;
-                InterfaceITAPI_Data.interfaceIT_Brightness_Enable(GetSelectedDeviceSession(), value);
-                this._brightnessEnabled = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
         private int _analogValue;
-        public int AnalogValue
-        {
-            get => this._analogValue;
-            set
-            {
-                this._analogValue = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
         private bool _analogEnabled;
-        public bool AnalogEnabled
-        {
-            get => this._analogEnabled;
-            set
-            {
-                this.GetValuesCancellationTokenSource = new CancellationTokenSource();
-                InterfaceITAPI_Data.interfaceIT_Analog_Enable(GetSelectedDeviceSession(), value);
-                if (value == true)
-                {
-                    this.AnalogValueThread = new Thread(o => this.GetValues(GetValuesCancellationTokenSource.Token));
-                    this.AnalogValueThread.Start();
-                }
-                else
-                {
-                    this.GetValuesCancellationTokenSource?.Cancel();
-                    this.AnalogValueThread = null;
-                    this.AnalogValue = 0;
-                }
-                this._analogEnabled = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
         private bool _loggingEnabled;
-        public bool LoggingEnabled
-        {
-            get => this._loggingEnabled;
-            set
-            {
-                InterfaceITAPI_Data.interfaceIT_EnableLogging(value);
-                if (value == true)
-                {
-                    System.Diagnostics.Process.Start("explorer.exe", Environment.ExpandEnvironmentVariables(@"%appdata%\TEKWorx Limited\interfaceIT API"));
-                }
-                this._loggingEnabled = value;
-                OnPropertyChanged();
-            }
-        }
 
+        [ObservableProperty]
         private string _featureNotSupported;
-        public string FeatureNotSupported
+
+        [RelayCommand]
+        private void SevenSegmentEnable()
         {
-            get => this._featureNotSupported;
-            set
+            if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_OUTPUT_7SEGMENT) == 0)
             {
-                this._featureNotSupported = value;
-                OnPropertyChanged();
+                this.FeatureNotSupported = "7 Segment not supported!";
+                return;
+            }
+            this.SevenSegmentEnabled = !this.SevenSegmentEnabled;
+            this.FeatureNotSupported = string.Empty;
+            if (this.SevenSegmentEnabled)
+            {
+                this.SevenSegmentPositions.Clear();
+                for (int i = DeviceList[GetSeletedController()].DeviceInfo.n7SegmentFirst; i <= DeviceList[GetSeletedController()].DeviceInfo.n7SegmentLast; i++)
+                {
+                    this.SevenSegmentSelectedPosition = 1;
+                    this.SevenSegmentPositions.Add(i);
+                }
+            }
+            _ = InterfaceITAPI_Data.interfaceIT_7Segment_Enable(GetSelectedDeviceSession(), this.SevenSegmentEnabled);
+        }
+
+        [RelayCommand]
+        private void DatalineEnable()
+        {
+            if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_OUTPUT_DATALINE) == 0)
+            {
+                this.FeatureNotSupported = "Dataline not supported!";
+                return;
+            }
+            this.DatalineEnabled = !this.DatalineEnabled;
+            this.FeatureNotSupported = string.Empty;
+            if (!this.DatalineEnabled)
+            {
+                _ = InterfaceITAPI_Data.interfaceIT_Dataline_Set(GetSelectedDeviceSession(), DeviceList[GetSeletedController()].DeviceInfo.nDatalineFirst, this.DatalineEnabled);
+                _ = InterfaceITAPI_Data.interfaceIT_Dataline_Enable(GetSelectedDeviceSession(), this.DatalineEnabled);
+                return;
+            }
+            _ = InterfaceITAPI_Data.interfaceIT_Dataline_Enable(GetSelectedDeviceSession(), this.DatalineEnabled);
+            _ = InterfaceITAPI_Data.interfaceIT_Dataline_Set(GetSelectedDeviceSession(), DeviceList[GetSeletedController()].DeviceInfo.nDatalineFirst, this.DatalineEnabled);
+        }
+
+        [RelayCommand]
+        private void BrightnessEnable()
+        {
+            if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_BRIGHTNESS) == 0)
+            {
+                this.FeatureNotSupported = "Brightness not supported!";
+                return;
+            }
+            this.BrightnessEnabled = !this.BrightnessEnabled;
+            this.FeatureNotSupported = string.Empty;
+            this.BrightnessValue = 0;
+            _ = InterfaceITAPI_Data.interfaceIT_Brightness_Enable(GetSelectedDeviceSession(), this.BrightnessEnabled);
+        }
+
+        [RelayCommand]
+        private void AnalogEnable()
+        {
+            if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_ANALOG_INPUT) == 0 &&
+                (DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_ANALOG16_INPUT) == 0)
+            {
+                this.FeatureNotSupported = "Analog input not supported!";
+                return;
+            }
+            this.AnalogEnabled = !this.AnalogEnabled;
+            this.FeatureNotSupported = string.Empty;
+
+            this.GetValuesCancellationTokenSource = new CancellationTokenSource();
+            _ = InterfaceITAPI_Data.interfaceIT_Analog_Enable(GetSelectedDeviceSession(), this.AnalogEnabled);
+            if (!this.AnalogEnabled)
+            {
+                this.GetValuesCancellationTokenSource?.Cancel();
+                this.AnalogValueThread = null;
+                this.AnalogValue = 0;
+                return;
+            }
+            this.AnalogValueThread = new Thread(o => this.GetValues(GetValuesCancellationTokenSource.Token));
+            this.AnalogValueThread.Start();
+        }
+
+        [RelayCommand]
+        private void LoggingEnable()
+        {
+            this.LoggingEnabled = !this.LoggingEnabled;
+            _ = InterfaceITAPI_Data.interfaceIT_EnableLogging(this.LoggingEnabled);
+            if (this.LoggingEnabled)
+            {
+                System.Diagnostics.Process.Start("explorer.exe", Environment.ExpandEnvironmentVariables(@"%appdata%\TEKWorx Limited\interfaceIT API"));
             }
         }
 
-
-        public OtherTestsViewModel()
+        public void Reset7SegmentDisplay()
         {
-            this.SevenSegmentEnableCommand = new RelayCommand(() =>
-            {
-                if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_OUTPUT_7SEGMENT) != 0)
-                {
-                    this.SevenSegmentEnabled = !this.SevenSegmentEnabled;
-                    this.FeatureNotSupported = string.Empty;
-                }
-                else
-                {
-                    this.FeatureNotSupported = "7 Segment not supported!";
-                }
-            });
-
-            this.DatalineEnableCommand = new RelayCommand(() =>
-            {
-                if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_OUTPUT_DATALINE) != 0)
-                {
-                    this.DatalineEnabled = !this.DatalineEnabled;
-                    this.FeatureNotSupported = string.Empty;
-                }
-                else
-                {
-                    this.FeatureNotSupported = "Dataline not supported!";
-                }
-            });
-
-            this.BrightnessEnableCommand = new RelayCommand(() =>
-            {
-                if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_BRIGHTNESS) != 0)
-                {
-                    this.BrightnessEnabled = !this.BrightnessEnabled;
-                    this.FeatureNotSupported = string.Empty;
-                }
-                else
-                {
-                    this.FeatureNotSupported = "Brightness not supported!";
-                }
-            });
-
-            this.AnalogEnableCommand = new RelayCommand(() =>
-            {
-                if ((DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_ANALOG_INPUT) != 0 ||
-                    (DeviceList[GetSeletedController()].DeviceInfo.dwFeatures & InterfaceIT_BoardInfo.Features.INTERFACEIT_FEATURE_SPECIAL_ANALOG16_INPUT) != 0)
-                {
-                    this.AnalogEnabled = !this.AnalogEnabled;
-                    this.FeatureNotSupported = string.Empty;
-                }
-                else
-                {
-                    this.FeatureNotSupported = "Analog input not supported!";
-                }
-            });
-
-            this.LoggingEnableCommand = new RelayCommand(() =>
-            {
-                this.LoggingEnabled = !this.LoggingEnabled;
-            });
+            _ = InterfaceITAPI_Data.interfaceIT_7Segment_Display(GetSelectedDeviceSession(), new string(' ', DeviceList[GetSeletedController()].DeviceInfo.n7SegmentCount), 1);
         }
 
-        //Get Analog Values Method
         public void GetValues(CancellationToken token)
         {
             int oldValue = 0;
             int noldValue = 0;
             while (this.AnalogEnabled)
             {
-                InterfaceITAPI_Data.interfaceIT_Analog_GetValue(GetSelectedDeviceSession(), 0, out int value);
+                _ = InterfaceITAPI_Data.interfaceIT_Analog_GetValue(GetSelectedDeviceSession(), 0, out int value);
                 if (value - oldValue >= 50 | value - oldValue <= -50 | oldValue == 0)
                 {
                     oldValue = value;
